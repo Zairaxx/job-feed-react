5a427a067dcad14296917bf20c7287f2329623fc
Zairaxx <brandontsegai@gmail.com>
2019-04-01 01:29:51 +0200

    fixed some indenting errors

--- src/components/JobList/JobList.js
+++ src/components/JobList/JobList.js
@@ -11,26 +11,28 @@ const JobList = ({ads, openAd}) => {
                 let {descr, id, company, title, experience, employment_type, locations, urls} = ads;
                 return (
                     <Fade left big>
-                    <JobListItem key={id} onClick={openAd}>
-                        <Flex direction="row" justify="space-between" >
-                            <div>
-                                <Flex direction="column" justify="center" alignItems="center">
-                                    <img height="48px" width="48px" src={company.logo} alt={company.name}/>
-                                    {company.website? <a href={company.website} rel="noopener noreferrer" target="_blank"><p>{company.name}</p></a>:<p> {company.name}</p>}
-                                </Flex>
-                                <Flex direction="column">
-                                    <p>{title}</p>
-                                    {locations[0].location.url ? <a href={locations[0].location.url} rel="noopener noreferrer" target="_blank"><p>{locations[0].location.text}</p></a> : <p>{locations[0].location.text}</p>}
-                                </Flex>
-                                <Flex direction="column">
-                                    <p> Experience: {experience}</p>
-                                    <p> Capacity: {employment_type}</p>
-                                </Flex>
-                            </div>
+                        <JobListItem key={id} onClick={openAd}>
+                            <Flex direction="row" justify="space-between" >
+                                <div>
+                                    <Flex direction="column" justify="center" alignItems="center">
+                                        <img height="48px" width="48px" src={company.logo} alt={company.name}/>
+                                        {company.website? <a href={company.website} rel="noopener noreferrer" target="_blank"><p>{company.name}</p></a>:<p> {company.name}</p>}
+                                    </Flex>
+                                    <Flex direction="column">
+                                        <p>{title}</p>
+                                        {locations[0].location.url ? <a href={locations[0].location.url} rel="noopener noreferrer" target="_blank"><p>{locations[0].location.text}</p></a> : <p>{locations[0].location.text}</p>}
+                                    </Flex>
+                                    <Flex direction="column">
+                                        <p> Experience: {experience}</p>
+                                        <p> Capacity: {employment_type}</p>
+                                    </Flex>
+                                </div>
                                 <CallToAction><a href={urls.ad} target="_blank" rel="noopener noreferrer">Apply For Job</a></CallToAction>
-                        </Flex>
-                        <JobListItemDesc dangerouslySetInnerHTML={{ __html: descr }}></JobListItemDesc>
-                    </JobListItem></Fade>)
+                            </Flex>
+                            <JobListItemDesc dangerouslySetInnerHTML={{ __html: descr }}></JobListItemDesc>
+                        </JobListItem>
+                    </Fade>
+                )
             })
         return(
             <React.Fragment>
